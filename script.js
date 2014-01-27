115a320be6b0b5066a0edde7a1071fb34dbb8722
WebFreak001 <janju007@web.de>
2014-01-27 21:55:02 +0100

    Fixed wrong variable name

--- script.js
+++ script.js
@@ -114,7 +114,7 @@ function checkFacebook()
 			xhttp = new ActiveXObject("Microsoft.XMLHTTP");
 		}
 		xhttp.open("GET", "https://graph.facebook.com/TheLockNLol/feed?limit=5&access_token=678452665531590|x3qFGSCtknwuL6CRSk8zAztx69Y", false);
-		xmlhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded; charset=ISO-8859-1;');
+		xhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded; charset=ISO-8859-1;');
 		xhttp.send();
 		var json = JSON.parse(xhttp.responeText);
 		var objects = json.data;
